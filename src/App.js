c49666bfe3c93346d48d30743f6cc918316cabc3
Tuguldur.bayrtsolmon@gmail.com <togoldor.bayrtsolmon@gmail.com>
2024-03-19 14:36:03 +0800

    Timer,Timeline

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import './App.css';
-import Header from './components/header/Header';
-import Home from './components/home/Home';
-import Event from './components/event/Event';
+import "./App.css";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import Event from "./components/event/Event";
+import HeaderWithTimer from "./components/header/HTimer";
+import CustomizedTimeline from "./components/header/timeLine";
 function App() {
+  const startDate = new Date(); // Start date
+  const endDate = new Date(startDate.getTime() + 24 * 60 * 60 * 1000); // End Date
   return (
     <>
-      <Header/>
-      <Home/>
-      <Event/>
+      <Header />
+      <Home />
+      <HeaderWithTimer
+        title="Hackathon дуустал:"
+        startDate={startDate}
+        endDate={endDate}
+      />
+      <div className="flex justify-center mt-40">
+        <h1 className="text-5xl font-bold text-center customFont">Timeline</h1>
+      </div>
+      <div className="flex mt-10 mb-20">
+        <CustomizedTimeline />
+      </div>
+      <Event />
     </>
   );
 }
