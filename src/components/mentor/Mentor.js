d086a03266aee062e0aad86c80a56e206ff31ae3
Munn Light <m.elbegsaikhan01@gmail.com>
2024-03-21 14:39:18 +0800

    Mentor saijruulalt hiiv

--- src/components/mentor/Mentor.js
+++ src/components/mentor/Mentor.js
@@ -4,6 +4,9 @@ import Card from "./card/Card";
 import men1 from "./imgs/men.png";
 
 const Mentor = () => {
+  const displayMentor = () => {
+    console.log("used display mentor");
+  };
   const [isHover, setIsHover] = useState(false);
   return (
     <>
@@ -30,12 +33,93 @@ const Mentor = () => {
             onMouseEnter={() => setIsHover(true)}
             onMouseLeave={() => setIsHover(false)}
           >
-            <Card url={men1} name="Mentor1" quote="Something about mentor" opa="chOpa" mgt={"mt-6"} bg={true} />
-            <Card url={men1} name="Mentor2" quote="Something about mentor" opa="chOpa" mgt={"mt-28"} />
-            <Card url={men1} name="Mentor3" quote="Something about mentor" opa="chOpa" mgt={"mt-12"} bg={true} />
-            <Card url={men1} name="Mentor4" quote="Something about mentor" opa="chOpa" mgt={"mt-20"} />
-            <Card url={men1} name="Mentor5" quote="Something about mentor" opa="chOpa" mgt={"mt-28"} bg={true} />
-            <Card url={men1} name="Mentor6" quote="Something about mentor" opa="chOpa" mgt={"mt-16"} />
+            <Card
+              url={men1}
+              name="Mentor1"
+              info={
+                <>
+                  <p>Education: SICT</p>
+                  <p>Job Title: Mentor job</p>
+                  <p>Some other informations</p>
+                </>
+              }
+              quote="Something about mentor"
+              opa="chOpa"
+              mgt={"pt-6"}
+              bg={true}
+            />
+            <Card
+              url={men1}
+              name="Mentor2"
+              info={
+                <>
+                  <p>Education: SICT</p>
+                  <p>Job Title: Mentor job</p>
+                  <p>Some other informations</p>
+                </>
+              }
+              quote="Something about mentor"
+              opa="chOpa"
+              mgt={"pt-28"}
+            />
+            <Card
+              url={men1}
+              name="Mentor3"
+              info={
+                <>
+                  <p>Education: SICT</p>
+                  <p>Job Title: Mentor job</p>
+                  <p>Some other informations</p>
+                </>
+              }
+              quote="Something about mentor"
+              opa="chOpa"
+              mgt={"pt-12"}
+              bg={true}
+            />
+            <Card
+              url={men1}
+              name="Mentor4"
+              info={
+                <>
+                  <p>Education: SICT</p>
+                  <p>Job Title: Mentor job</p>
+                  <p>Some other informations</p>
+                </>
+              }
+              quote="Something about mentor"
+              opa="chOpa"
+              mgt={"pt-20"}
+            />
+            <Card
+              url={men1}
+              name="Mentor5"
+              info={
+                <>
+                  <p>Education: SICT</p>
+                  <p>Job Title: Mentor job</p>
+                  <p>Some other informations</p>
+                </>
+              }
+              quote="Something about mentor"
+              opa="chOpa"
+              mgt={"pt-28"}
+              bg={true}
+            />
+            <Card
+              url={men1}
+              name="Mentor6"
+              info={
+                <>
+                  <p>Education: SICT</p>
+                  <p>Job Title: Mentor job</p>
+                  <p>Some other informations</p>
+                </>
+              }
+              quote="Something about mentor"
+              opa="chOpa"
+              mgt={"pt-16"}
+            />
           </div>
         )}
       </div>
--- src/components/mentor/card/Card.js
+++ src/components/mentor/card/Card.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Card.css";
 
-const Card = ({ url, name, quote, mgt, bg, opa }) => {
+const Card = ({ url, name, quote, mgt, bg, opa, info }) => {
+  const [isClicked, SetIsClicked] = useState(false);
   return (
     <>
-      <div className={`${mgt} ${opa} opaAlwaysOne`}>
+      {isClicked && (
+        <div className="selected">
+          <div className={bg ? "bg-emerald-400" : "bg-red-400"}>
+            <img src={url} />
+          </div>
+          <div className="font-bold text-black text-left inline-block align-middle">
+            <h1 className="mb-3 text-3xl">{name}</h1>
+            <p className="text-2xl">{quote}</p>
+            <p className="text-2xl">{info}</p>
+          </div>
+          <div
+            onClick={() => {
+              SetIsClicked(false);
+            }}
+            className="absolute right-3 top-2 p-3 cursor-pointer text-black font-bold hover:text-opacity-55"
+          >
+            X
+          </div>
+        </div>
+      )}
+      {/* {!isClicked && ( */}
+      <div
+        className={`${mgt} ${opa} opaAlwaysOne cursor-pointer`}
+        onClick={() => {
+          SetIsClicked(true);
+        }}
+      >
         <div
           className={
             bg ? "bg-gradient-to-b from-emerald-500 to-emerald-200 own" : "bg-gradient-to-b from-red-500 to-red-200 own"
-            // "own"
           }
         >
           <img src={url} alt="mentor" className="pt-7" />
@@ -18,6 +44,7 @@ const Card = ({ url, name, quote, mgt, bg, opa }) => {
           <p className="text-sm">{quote}</p>
         </div>
       </div>
+      {/* )} */}
     </>
   );
 };
