405208909ced9e5b6fcf8d708aea7602616ac3b6
The Margad <122777402+TheMargadXR@users.noreply.github.com>
2024-04-01 15:29:20 +0800

    Sponser respo

--- src/components/sponser/Sponser.js
+++ src/components/sponser/Sponser.js
@@ -1,46 +1,40 @@
 import React from "react";
-import InfiniteScroll from "./InfiniteScroll";
 import TextTyping from "./TextTyping";
 import Sponser_1 from "./assets/sponser_1.png";
 import Sponser_2 from "./assets/sponser_2.png";
 
 function Sponser() {
   return (
-    <section className="h-screen w-full bg-main  text-black">
-      <div className="">
-        <div className="p-20">
+    <section className="bg-main h-screen text-black">
+      <div className="p-20 ">
+        <div className="m-6 sm:m-24">
           <div className="flex justify-center text-4xl">
             <h2 className="text-[#f4f4f4] text-4xl font-mono">
-              {/* Main |<span className="text-white"> Sponser</span> */}
-              <TextTyping text="Main | Sponser" />
+              <TextTyping text="Main | Sponsor" />
             </h2>
           </div>
-
-          <div className=" flex justify-center items-center flex-col mx-52 py-14">
+          <div className="flex justify-center items-center flex-col sm:mx-0 mx-5 py-14">
             <div className="w-full grid gap-5">
               <div className="grid grid-cols-1 gap-4 rounded-md">
                 <div className="sponser-card">
-                  <img src={Sponser_2} className="imgSize"></img>
+                  <img src={Sponser_2} className="imgSize" alt="Sponser 1" />
                 </div>
               </div>
               <div className="flex justify-center py-5 items-center text-[#f4f4f4]">
-                <TextTyping text="Sponser" />
+                <TextTyping text="Sponsor" />
               </div>
-              <div className="grid grid-cols-3 gap-4 rounded-md">
+              <div className="grid grid-cols-1 gap-4 rounded-md sm:grid-cols-2">
                 <div className="sponser-card">
-                  <img src={Sponser_2} className="imgSize"></img>
+                  <img src={Sponser_2} className="imgSize" alt="Sponser 2" />
                 </div>
+
                 <div className="sponser-card">
-                  <img src={Sponser_1} className="imgSize"></img>
-                </div>
-                <div className="sponser-card">
-                  <img src={Sponser_2} className="imgSize"></img>
+                  <img src={Sponser_2} className="imgSize" alt="Sponser 3" />
                 </div>
               </div>
             </div>
           </div>
         </div>
-        {/* <InfiniteScroll /> */}
       </div>
     </section>
   );
